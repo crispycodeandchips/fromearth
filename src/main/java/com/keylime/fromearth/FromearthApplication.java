03ee59408bf660dad18041320a8ca018414a9ccf
crispycodeandchips <bb9e.bb8.c3po.r2d2@gmail.com>
2021-10-11 15:36:09 +0900

    init commit

---
+++ src/main/java/com/keylime/fromearth/FromearthApplication.java
@@ -0,0 +1,13 @@
+package com.keylime.fromearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromearthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromearthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keylime/fromearth/FromearthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keylime.fromearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromearthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
